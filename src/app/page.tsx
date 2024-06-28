cb9560575188acce3eef9972f6c48abf6ddc2212
haoolii <unnhao@gmail.com>
2024-06-28 18:54:57 +0800

    feat: add base test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,51 @@
 import ButtonUsage from "@/components/ButtonUsage";
-import { Table } from "@mui/joy";
+import {
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Grid,
+  Input,
+  Table,
+} from "@mui/joy";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="container mx-auto py-12">
+    <main className="container mx-auto py-12 px-10">
+      <div className="mb-12">
+        <Grid container columnSpacing={4} rowSpacing={2}>
+          <Grid xs={6}>
+            <FormControl>
+              <FormLabel>定投币种</FormLabel>
+              <Input placeholder="定投币种" />
+              {/* <FormHelperText>This is a helper text.</FormHelperText> */}
+            </FormControl>
+          </Grid>
+          <Grid xs={6}>
+            <FormControl>
+              <FormLabel>定投金额</FormLabel>
+              <Input placeholder="定投金额" />
+              {/* <FormHelperText>This is a helper text.</FormHelperText> */}
+            </FormControl>
+          </Grid>
+          <Grid xs={6}>
+            <FormControl>
+              <FormLabel>开始时间</FormLabel>
+              <Input placeholder="开始时间" />
+              {/* <FormHelperText>This is a helper text.</FormHelperText> */}
+            </FormControl>
+          </Grid>
+          <Grid xs={6}>
+            <FormControl>
+              <FormLabel>结束时间</FormLabel>
+              <Input placeholder="结束时间" />
+              {/* <FormHelperText>This is a helper text.</FormHelperText> */}
+            </FormControl>
+          </Grid>
+        </Grid>
+      </div>
       <div>
-      {/* 日期	定投金额(USDT)	买入价格(USDT)	买入数量(--)	总计投入(USDT)	总计数量(--)	定投均价(USDT) */}
+        {/* 日期	定投金额(USDT)	买入价格(USDT)	买入数量(--)	总计投入(USDT)	总计数量(--)	定投均价(USDT) */}
         <Table borderAxis="both">
           <thead>
             <tr>
@@ -21,33 +60,40 @@ export default function Home() {
           </thead>
           <tbody>
             <tr>
-              <th scope="row">Margaret Nguyen</th>
-              <td>427311</td>
-              <td>
-                <time dateTime="2010-06-03">June 3, 2010</time>
-              </td>
-              <td>n/a</td>
-              <td>0.00</td>
+              <th scope="row">2021-01-01</th>
+              <td>1000</td>
+              <td>28921.07</td>
+              <td>0.034576</td>
+              <td>1000</td>
+              <td>0.034576</td>
+              <td>28921.07</td>
             </tr>
             <tr>
-              <th scope="row">Edvard Galinski</th>
-              <td>533175</td>
-              <td>
-                <time dateTime="2011-01-13">January 13, 2011</time>
-              </td>
-              <td>
-                <time dateTime="2017-04-08">April 8, 2017</time>
-              </td>
-              <td>37.00</td>
+              <th scope="row">2021-01-01</th>
+              <td>1000</td>
+              <td>28921.07</td>
+              <td>0.034576</td>
+              <td>1000</td>
+              <td>0.034576</td>
+              <td>28921.07</td>
             </tr>
             <tr>
-              <th scope="row">Hoshi Nakamura</th>
-              <td>601942</td>
-              <td>
-                <time dateTime="2012-07-23">July 23, 2012</time>
-              </td>
-              <td>n/a</td>
-              <td>15.00</td>
+              <th scope="row">2021-01-01</th>
+              <td>1000</td>
+              <td>28921.07</td>
+              <td>0.034576</td>
+              <td>1000</td>
+              <td>0.034576</td>
+              <td>28921.07</td>
+            </tr>
+            <tr>
+              <th scope="row">2021-01-01</th>
+              <td>1000</td>
+              <td>28921.07</td>
+              <td>0.034576</td>
+              <td>1000</td>
+              <td>0.034576</td>
+              <td>28921.07</td>
             </tr>
           </tbody>
         </Table>
